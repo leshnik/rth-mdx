6b327b7c3677588f9b17c5979be2bd47c2d9268a
Morton Potash <mortonpotash@gmail.com>
2020-08-01 06:50:50 +0200

    add styled components and theme

---
+++ src/themes/theme.js
@@ -0,0 +1,41 @@
+export default {
+    fonts: {
+        main: "lora, serif",
+        art: "amatic sc",
+        sans: "muli, sans-serif",
+    },
+    colors: {
+        art: "#02b45b",
+        sophro: "#7551e1",
+        spectacle: "f25f5f",
+        hover: "#a43740",
+        domicile: "darkgoldenrod",
+        main1: "hsl(207,70%,59%)",
+        main2: "hsl(207,70%,94%)",
+        dark1: "hsl(227,2%,12%)",
+        dark2: "hsl(227,2%,26%)",
+        dark3: "hsl(227,2%,64%)",
+        light1: "hsl(0,0%,97%)",
+        light2: "(0,0%,99%)",
+    },
+    breakpoints: {
+        mobile: "only screen and (max-width: 50rem)",
+        tablet: "only screen and (max-width: 65rem)",
+    },
+    spacings: {
+        xxSmall: ".25rem",
+        xSmall: ".5rem",
+        small: "1rem",
+        medium: "2rem",
+        large: "3rem",
+        xlarge: "4rem",
+        xxlarge: "6rem",
+    },
+    animations: {
+        button: "box-shadow 0.3s ease",
+        link: "color 0.2s ease",
+    },
+    shadows: {
+        shadow1: "0px 5px 20px rgba(30, 30, 31, 0.05)"
+    },
+}
