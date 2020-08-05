a831ac45fa72fc83cbde8152c504de2368bac75f
Morton Potash <mortonpotash@gmail.com>
2020-08-05 09:15:25 +0200

    add Content components

---
+++ src/elements/ContentElements.js
@@ -0,0 +1,18 @@
+import Styled from 'styled-components'
+
+export const ContentWrapper = Styled.main`
+    grid-column: 4 / span 8;
+    grid-row: 3 / span 5;
+    background-color: ${props => props.theme.colors.light2};
+    padding: ${props => `${props.theme.spacings.xlarge} ${props.theme.spacings.xxlarge}`};
+    box-shadow: ${props => props.theme.shadows.shadow1};
+    z-index: 10;
+
+    @media ${props => props.theme.breakpoints.tablet} {
+        grid-column: 2 / span 6;
+    };
+    @media ${props => props.theme.breakpoints.mobile} {
+        padding: ${props => `${props.theme.spacings.medium} ${props.theme.spacings.large}`};
+    }
+
+`
